864918bedcd6026d8654d21ea9cd2048ae9ecb8b
Aarsh3nic <50718440+Aarsh3nic@users.noreply.github.com>
2020-07-28 08:29:13 -0400

    Add files via upload

---
+++ assets/data/myCars.ts
@@ -0,0 +1,26 @@
+import { CarInfo } from "../../app/carInfo";
+
+export const MYCARS : CarInfo[] = [
+
+    {carMake: "Bugatti",
+        carModel: "Veyron",
+        carBodyType: "Sports",
+        carColor: "Blue",
+        carPicture: "assets/images/veyron.jpg"},
+        {carMake: "Audi",
+        carModel: "R8",
+        carBodyType: "Sports",
+        carColor: "White",
+        carPicture: "assets/images/r8.jpg"},
+        {carMake: "Porche",
+        carModel: "Cayenne",
+        carBodyType: "SUV",
+        carColor: "Red",
+        carPicture: "assets/images/cayenne.jpg"},
+        {carMake: "Jaguar",
+        carModel: "XK",
+        carBodyType: "Convertible",
+        carColor: "Silver",
+        carPicture: "assets/images/xk.jpg"}
+
+]
